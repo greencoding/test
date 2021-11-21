493e240fb3ff54ed791945e3db04a859029204af
zersog <1113463764@qq.com>
2021-11-21 19:51:24 +0800

    【add】master push1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("hello world");
-        System.out.println("hello git") ;
-        System.out.println("hello world  hello git") ;
-        System.out.println("hello  hot-fix") ;
-        System.out.println("hello  hot-fix test") ;
-        System.out.println("hello  master test") ;
+        System.out.println("hello git");
+        System.out.println("hello world  hello git");
+        System.out.println("hello  hot-fix");
+        System.out.println("hello  hot-fix test");
+        System.out.println("hello  master test");
         System.out.println("push test");
-	System.out.println("pull test");
+        System.out.println("pull test");
     }
 }
